b66d575f6dd039f0c071161673aed6f5a1b2e1f8
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-10-03 16:41:47 +0000

    new file created

---
+++ madlib.py
@@ -0,0 +1,26 @@
+"""
+Name: William Nathan
+Date: 10/3/24
+Description: Mad Lib Practice
+"""
+noun1 = input("Give me a noun: ")
+fantasy_class = input("Give me a fantasy class: ")
+past_adj = input("Give me an adjective with past tense: ")
+verbing = input("Give me a verb that ends with -ing: ")
+verb2ing = input("Give me another verb that ends with -ing: ")
+noun2 = input("Give me another noun: ")
+verb3 = input("Give me another verb: ")
+adjective2 = input("Give me an Adjective: ")
+verb4 = input("Please give me another Verb: ")
+adjective3 = input("Please give me another adjective: ")
+adverb2 = input ("Please give me another adverb: ")
+onomatopoeia_noise = ("Please give me a noise, such as an onomatopoeia: ")
+
+print(f"There once was an adventurer named {noun1}. {noun1} was a {fantasy_class} who had {past_adj} all across the land, {verbing}/
+    people and {verb2ing} monsters for whoever had asked them. One day, they found themselves in {noun2}. Stuck, they / 
+    needed to find a way out. {Noun3} crawled all around them on the {adjective2} floors. The {noun4} filled with a {adjective3}/
+    smell. They first tried to {verb3}, but the stone was too {adjective2}. They then tried to {verb4},/
+    but their finger's had a difficult time. Finally, they noticed the door was slightly {adjective3}, and decided this was /
+    their way out. They {adverb2} the door to their cell, which finally worked. The lock came off with a (onomatopoeia_noise), /
+    and they returned to their adventuring life. 
+      ")
