5f9b86b0bf7e4b4695c4ff4c575717d52ee62fac
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-10-14 15:52:00 +0000

    finished project

--- madlib.py
+++ madlib.py
@@ -14,6 +14,7 @@ verb = input("Give me a verb that ends with -ing: ")
 verb2 = input("Give me another verb that ends with -ing: ")
 noun2 = input("Give me another noun: ")
 noun3 = input("Give me a noun: ")
+verb5 = input("Give me a verb: ")
 verb3 = input("Give me another verb: ")
 adjective2 = input("Give me an Adjective: ")
 noun4 = input("Give me another Noun: ")
@@ -27,7 +28,7 @@ print(name)
 print(date)
 print(f"There once was an adventurer named {noun1}. {noun1} was a {fantasy_class} who had {past_adj} all across the land, {verb}\
   people and {verb2} monsters for whoever had asked them. One day, they found themselves in {noun2}. Stuck, they\
-  needed to find a way out. {noun3} crawled all around them on the {adjective2} floors. The {noun4} filled with a {adjective3}\
+  needed to find a way out. {noun3} {verb5} all around them on the {adjective2} floors. The {noun4} filled with a {adjective3}\
   smell. They first tried to {verb3}, but the stone was too {adjective2}. They then tried to {verb4}\
   but their finger's had a difficult time. Finally, they noticed the door was slightly {adjective3}, and decided this was\
   their way out. They {adverb2} the door to their cell, which finally worked. The lock came off with a {onomatopoeia_noise},\
